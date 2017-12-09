ceb189f7c0dd3af1452564b5cfce36843fb4e62c
= <luc.potage@gmail.com>
2017-12-09 10:55:21 +0100

    Replace spread operator

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import katex from 'katex'
+'use strict';
 
-export default {
-  install: function (Vue, options) {
-    Vue.directive('katex', function (el, binding) {
-      var displayStyle = binding.arg === 'display' ? true : false
+var katex = require('katex');
 
-      if (binding.value.expression) {
-        if (binding.value.options) {
-          katex.render(binding.value.expression, el, {
-            displayMode: displayStyle,
-            ...binding.value.options
-          })
-        } else {
-          katex.render(binding.value.expression, el, {
-            displayMode: displayStyle
-          })
-        }
+var install = function (Vue) {
+  Vue.directive('katex', function (el, binding) {
+    var displayStyle = binding.arg === 'display' ? true : false;
+
+    if (binding.value.expression) {
+      if (binding.value.options) {
+        katex.render(binding.value.expression, el, Object.assign(
+          { displayMode: displayStyle },
+          binding.value.options
+        ));
       } else {
-        katex.render(binding.value, el, {
+        katex.render(binding.value.expression, el, {
           displayMode: displayStyle
-        })
+        });
       }
-    })
-  }
+    } else {
+      katex.render(binding.value, el, {
+        displayMode: displayStyle
+      });
+    }
+  })
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
 }
