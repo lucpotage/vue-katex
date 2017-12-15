67b2e1fc6764ba98142ba5e9b05881dd8114a2e5
Luc Potage <luc.potage@gmail.com>
2017-12-15 16:30:53 +0100

    Use import and export

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-'use strict';
+import katex from 'katex'
 
-var katex = require('katex');
-
-var install = function (Vue) {
-  Vue.directive('katex', function (el, binding) {
-    var displayStyle = binding.arg === 'display' ? true : false;
-
-    if (binding.value.expression) {
-      if (binding.value.options) {
-        katex.render(binding.value.expression, el, Object.assign(
-          { displayMode: displayStyle },
-          binding.value.options
-        ));
+export default {
+  install (Vue) {
+    Vue.directive('katex', function (el, binding) {
+      var displayStyle = binding.arg === 'display' ? true : false;
+  
+      if (binding.value.expression) {
+        if (binding.value.options) {
+          katex.render(binding.value.expression, el, Object.assign(
+            { displayMode: displayStyle },
+            binding.value.options
+          ));
+        } else {
+          katex.render(binding.value.expression, el, {
+            displayMode: displayStyle
+          });
+        }
       } else {
-        katex.render(binding.value.expression, el, {
+        katex.render(binding.value, el, {
           displayMode: displayStyle
         });
       }
-    } else {
-      katex.render(binding.value, el, {
-        displayMode: displayStyle
-      });
-    }
-  })
+    })
+  }
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
