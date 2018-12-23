cef982727e8bae74df89c3c88c1f694e6f92da13
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-23 11:18:50 -0500

    Using vue cli for config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/recommended", "eslint:recommended"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/component-name-in-template-casing": 0,
+    "linebreak-style": 0,
+    "max-len": 0
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div v-katex:display="'\\frac{a_i}{1+x}'" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+</style>
