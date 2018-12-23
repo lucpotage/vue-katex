bcef8954b038aac5efcb947133dd2d3f3edf75ff
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-23 14:39:30 -0500

    Adding unit testing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <div v-katex:display="'\\frac{a_i}{1+x}'" />
+    <div v-katex="'\\frac{a_i}{1+x}'" />
+    <div v-katex="{ expression: '\\frac{a_i}{1+x}', options: { throwOnError: false }}" />
   </div>
 </template>
 
