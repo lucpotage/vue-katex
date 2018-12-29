fd545373739016dbd910ae31e6a94a69a75b01c0
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-29 12:42:00 -0500

    Add KatexElement to plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,10 @@
 import katexDirective from './directives/katex-directive';
+import KatexElement from './components/KatexElement.vue';
 
 const plugin = {
   install(Vue) {
     Vue.directive(katexDirective.name, katexDirective.directive);
+    Vue.component(KatexElement.name, KatexElement);
   },
 };
 
