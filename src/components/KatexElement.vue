1a90902cc6f1db732227737c582f042f5571b87a
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-28 23:36:57 -0500

    Add katex element

---
+++ src/components/KatexElement.vue
@@ -0,0 +1,77 @@
+<script>
+import katex from 'katex';
+
+export default {
+  name: 'KatexElement',
+  props: {
+    expression: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    displayMode: {
+      type: Boolean,
+      default: false,
+    },
+    throwOnError: {
+      type: Boolean,
+      default: false,
+    },
+    errorColor: {
+      type: String,
+      default: '',
+    },
+    macros: {
+      type: Object,
+      default: null,
+    },
+    colorIsTextColor: {
+      type: Boolean,
+      default: false,
+    },
+    maxSize: {
+      type: Number,
+      default: Infinity,
+    },
+    maxExpand: {
+      type: Number,
+      default: 1000,
+    },
+    allowedProtocols: {
+      type: Array,
+      default: () => (['http', 'https', 'mailto', '_relative']),
+    },
+    strict: {
+      type: [Boolean, String, Function],
+      default: 'warn',
+    },
+  },
+  computed: {
+    innerOptions() {
+      return Object.assign({},
+          {
+            displayMode: this.displayMode,
+            throwOnError: this.throwOnError,
+            errorColor: this.errorColor,
+            macros: this.macros,
+            colorIsTextColor: this.colorIsTextColor,
+            maxSize: this.maxSize,
+            maxExpand: this.maxExpand,
+            allowedProtocols: this.allowedProtocols,
+            strict: this.strict,
+          });
+    },
+    math() {
+      return katex.renderToString(this.expression, this.innerOptions);
+    },
+  },
+  render(h) {
+    const element = this.displayMode ? 'div' : 'span';
+    return h(element, {
+      domProps: {
+        innerHTML: this.math,
+      },
+    });
+  },
+};
+</script>
